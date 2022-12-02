aae44eb8609370b06183fd1bab971ec960221116
Zoya29-Tabaasum <114081402+Zoya29-Tabaasum@users.noreply.github.com>
2022-12-02 22:37:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,84 +1,17 @@
-// import React, {useEffect, useState} from 'react'
-// import '../styles/App.css';
-
-// const App = () => {
-// //code here 
-// const [active,setActive]=useState(true);
-// const [className,setClassName]=useState('redColor');
-//  useEffect(()=>
-//  {
-//   if(active)
-//   {
-//     setClassName("redColor");
-//   }
-//   if(!active)
-//   {
-//     setClassName("blueColor");
-//   }
-//  },[active])
-//  const onButtonClick=()=>{
-
-//   setActive(!active);
-  
-//   }
-//   return (
-//     <div id="main">
-      
-//      <p className={className}>Newton School</p>
-
-//       <button id='button' onClick={onButtonClick}>Change Style</button>
-//     </div>
-//   )
-// }
-
-
-// export default App;
-
-
 import React, {useEffect, useState} from 'react'
-
 import '../styles/App.css';
 
 const App = () => {
+//code here 
+const [setColor] = useState("red");
 
-const [value,setValue]=useState(true)
-
-const [className,setClassName]=useState('redColor')
-
-const onButtonClick=()=>{
-
-setValue(!value);
-
+  return (
+    <div id="main">
+      <p className={'redColor'} >Newton School</p>
+      <button id='button' onClick={() => setColor('blue')}>Change Style</button>
+    </div>
+  )
 }
 
-useEffect(()=>{
-
-if(value){
-
-setClassName('redColor')
-
-}
-
-if(!value){
-
-setClassName('blueColor')
-
-}
-
-},[value])
-
-return (
-
-<div id="main">
-
-<p className={className} >Newton School</p>
-
-<button id='button' onClick={onButtonClick}>Change Style</button>
-
-</div>
-
-)
-
-}
 
 export default App;
